4beae80aa8033ed2bde3c43a9c59a1c285cc2f98
anlaptev <anlaptev@gmail.com>
2024-11-20 13:28:17 +0300

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,6 @@
+
+number_of_completed_DZ=12
+number_of_hours_spent=1.5
+course_name='Python'
+time_per_task=number_of_hours_spent/number_of_completed_DZ
+print('Курс: '+course_name+', всего задач: '+str(number_of_completed_DZ)+', затрачено часов: '+str(number_of_hours_spent)+', среднее время выполнения '+str(time_per_task)+' часа')
